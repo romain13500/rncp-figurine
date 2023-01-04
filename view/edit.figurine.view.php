6b015ac4c17cb3b4c93de4f4e2be98c207c2a68f
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-01-04 09:51:00 +0100

    creation fichier edit figurine

---
+++ view/edit.figurine.view.php
@@ -0,0 +1,48 @@
+<?php 
+    ob_start() 
+?> 
+<p class="text-warning text-center">Voici le formulaire</p>
+
+        <!-----  DEBUT FORMULAIRE EDIT GAME ----->
+
+<form  method="POST" action="<?= URL ?>dragonballadmin/editvalid">
+
+<!----- input recuperant le titre du jeu dans bdd  ----->
+<div class="form-group">    
+    <label for="image" class="mb-3">Image</label>
+     <input type="file" class="form-control" value="<?= $dragonball->getImage()?>" name="image" id="image"> <!-- affichage du titre du jeu dans input -->
+</div>
+
+<!----- input recuperant le nombre de joueurs du jeu dans bdd  ----->
+<div class="form-group mt-5">    
+    <label for="nbPlayers" class="mb-3">Nom</label>
+    <input type="text" class="form-control" name="name" value="<?= $dragonball->getName()?>" id="name"> <!-- affichage du nbPlayers du jeu dans input -->
+</div>
+
+<!----- input recuperant le prix du jeu dans bdd  ----->
+<div class="form-group">
+    <label for="price" class="mb-3">Prix</label>
+    <input type="number" class="form-control" name="price" value="<?= $dragonball->getPrice()?>">
+</div>
+
+<div class="form-group">
+    <input type="hidden" class="form-control" name="id" value="<?= $dragonball->getId()?>" >
+</div>
+
+     <button type="submit" class="btn btn-primary my-5" >Ajouter</button>
+</form> 
+
+        <!----- END FORM ----->
+
+
+
+
+
+<?php
+
+ //  lis et efface la temporisation 
+$content= ob_get_clean();
+$title=" Editer : " . $dragonball->getName();
+require_once "base.html.php";
+
+?>
