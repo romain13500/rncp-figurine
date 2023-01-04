63669de194014ab149c8985f2ccc4aa20d3f2ff5
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-01-04 09:53:12 +0100

    update edit figurine

--- view/edit.figurine.view.php
+++ view/edit.figurine.view.php
@@ -10,23 +10,23 @@
 <!----- input recuperant le titre du jeu dans bdd  ----->
 <div class="form-group">    
     <label for="image" class="mb-3">Image</label>
-     <input type="file" class="form-control" value="<?= $dragonball->getImage()?>" name="image" id="image"> <!-- affichage du titre du jeu dans input -->
+     <input type="file" class="form-control" value="<?= $figurine->getImage()?>" name="image" id="image"> <!-- affichage du titre du jeu dans input -->
 </div>
 
 <!----- input recuperant le nombre de joueurs du jeu dans bdd  ----->
 <div class="form-group mt-5">    
     <label for="nbPlayers" class="mb-3">Nom</label>
-    <input type="text" class="form-control" name="name" value="<?= $dragonball->getName()?>" id="name"> <!-- affichage du nbPlayers du jeu dans input -->
+    <input type="text" class="form-control" name="name" value="<?= $figurine->getName()?>" id="name"> <!-- affichage du nbPlayers du jeu dans input -->
 </div>
 
 <!----- input recuperant le prix du jeu dans bdd  ----->
 <div class="form-group">
     <label for="price" class="mb-3">Prix</label>
-    <input type="number" class="form-control" name="price" value="<?= $dragonball->getPrice()?>">
+    <input type="number" class="form-control" name="price" value="<?= $figurine->getPrice()?>">
 </div>
 
 <div class="form-group">
-    <input type="hidden" class="form-control" name="id" value="<?= $dragonball->getId()?>" >
+    <input type="hidden" class="form-control" name="id" value="<?= $figurine->getId()?>" >
 </div>
 
      <button type="submit" class="btn btn-primary my-5" >Ajouter</button>
@@ -42,7 +42,7 @@
 
  //  lis et efface la temporisation 
 $content= ob_get_clean();
-$title=" Editer : " . $dragonball->getName();
+$title=" Editer : " . $figurine->getName();
 require_once "base.html.php";
 
 ?>
