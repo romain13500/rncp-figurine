8732ae3143fb14d4e178bd756b470fb6b2c62531
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-01-08 19:48:18 +0100

    update font

--- view/home.view.php
+++ view/home.view.php
@@ -7,52 +7,12 @@ ob_start();
 <p> Accueil</p>
 
 <div class="container">
-  <div class="row">
-    <h2>top vente</h2>
+    <div class="block">
+        <div class="row">
+        
+            <h2 class="mt-3 mb-3 text-danger">TOP VENTE</h2>
 
-        <div class="col-md-4 border-right">
-            <div class="cards">
-           <div class=" second bg-primary p-4 text-center">
-                    <img class="w-75 rounded" src="image/dragonball/gogeta.jpeg"/>
-                    <hr>
-                    <p class="line2">Gogeta</p>
-                    <p>99€</p> 
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4 border-right">
-            <div class="cards">
-           <div class=" second bg-primary p-4 text-center">
-                    <img class="w-75 rounded" src="https://www.mangatori.fr/1800179-large_default/megahouse-89537-one-piece-pop-marco-phoenix-figure-rerun.jpg"/>
-                    <hr>
-                    <p class="line2">Marco Pheonix</p>
-                    <p>390€</p>
-                    
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-md-4 border-right">
-                <div class="cards">
-                    
-               
-                <div class="first bg-primary p-4 text-center">
-                    <img class="w-75 rounded" src="https://www.mangatori.fr/1707714-large_default/banpresto-ban18406-grandista-nero-naruto-uzumaki-manga-dimensions.jpg" />
-                    <hr>
-                    <p>Naruto</p>
-                <p class="line1">79€</p>
-                    
-                </div>
-
-                 </div>
-                  
-           </div>
-  </div>
-</div>
-<!-- <div class ="container ">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -61,7 +21,7 @@ ob_start();
     <div class="carousel-inner">
 
         <div class="carousel-item active">
-            <div class="card m-auto border-dark shadow-lg p-3 mb-5 rounded bg-primary" style="width: 18rem;">
+            <div class="cards m-auto border-dark shadow-lg p-3 mb-5 rounded bg-primary" style="width: 17rem;">
                 <img  src="image/dragonball/goku.jpeg" class="card-img-top" alt="...">
                 <hr>
                 <div class="card-body">
@@ -73,7 +33,7 @@ ob_start();
         </div>
 
         <div class="carousel-item">
-            <div class="card m-auto border-dark shadow-lg p-3 mb-5 bg-primary rounded" style="width: 18rem;">
+            <div class="cards m-auto border-dark shadow-lg p-3 mb-5 bg-primary rounded" style="width: 17rem;">
                 <img src="image/naruto/naruto.jpeg" class="card-img-top" alt="...">
                 <hr>
                 <div class="card-body">
@@ -85,7 +45,7 @@ ob_start();
         </div>
 
         <div class="carousel-item">
-        <div class="card m-auto border-dark shadow-lg p-3 mb-5 bg-primary rounded" style="width: 18rem;">
+        <div class="cards m-auto border-dark shadow-lg p-3 mb-5 bg-primary rounded" style="width: 17rem;">
                 <img src="image/onepiece/luffy.jpeg" class="card-img-top" alt="...">
                 <hr>
                 <div class="card-body">
@@ -95,10 +55,7 @@ ob_start();
                 </div>
             </div>
         </div>
-
-    </div>
-
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
@@ -106,9 +63,13 @@ ob_start();
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
+            
+        </div>
     </div>
+</div>
 
-</div> -->
+
+</div> 
 
 <div class="container mt-5">
     <div>
